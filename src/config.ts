945ee4dc1486ee2c8b046b3b162da440ce777798
ericallansolutions <eric.manosalva@allansolutions.com>
2025-05-12 16:09:42 -0500

    fix: change output file location

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export function getProjectPaths() {
     const inputProjectPath = argProject ? argProject.split('=')[1] : null;
     const projectRoot = inputProjectPath ? path.resolve(inputProjectPath) : path.resolve(__dirname, './');
     const componentsDir = path.join(projectRoot, 'components');
-    const outputFile = path.join(projectRoot, 'unused-components.json');
+    const outputFile = path.join('.', 'unused-components.json');
 
     return { projectRoot, componentsDir, outputFile };
 }
